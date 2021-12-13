b290ce8075afee7c5529a935ca6bab5f29b62471
Valentyn Kondratiev <Valentyn_Kondratiev@epam.com>
2021-12-14 00:23:05 +0200

    working prototype v1.1

--- src/index.js
+++ src/index.js
@@ -1,29 +1,62 @@
 'use strict'
-class cells {
-    constructor(color = `white`) {
-        this.color = color;
-        this.isDead = color === `white` ? true : false;
+class Cell {
+    constructor(isDead = true) {
+        this.isDead = isDead;
     }
 
-    complementaryPairs = {
-        black: [`black`, `white`],
-        white: [`white`, `black`]
+    clone(){
+      return new Cell(this.isDead);
     }
 
-    switchPair() {
-        this.color = this.complementaryPairs[color][!this.isDead];
+    kill() {
+        if (!this.isDead) this.isDead = true;
     }
 
-    makeDead() {
-        this.isDead = true;
-        this.switchPair();
-    }
-
-    makeAlive() {
-        this.isDead = false;
-        this.switchPair();
+    resurrect() {
+        if (this.isDead) this.isDead = false;
     }
 }
+
+class Colors  {
+    constructor(){
+        this.deadColor = `rgb(255, 255, 255)`
+        this.aliveColor = `rgb(0, 0, 0)`
+        this.borderColor = `rgb( 100, 100, 100)`
+    }
+
+    setColor = (isDead) => {
+        return isDead ? this.deadColor : this.aliveColor;
+    }
+}
+
+class AnimationFrame {
+    constructor(fps = 60, animate) {
+      this.requestID = 0;
+      this.fps = fps;
+      this.animate = animate;
+    }
+
+    stop() {
+        cancelAnimationFrame(this.requestID);
+      }
+  
+    start() {
+      let then = performance.now();
+      const interval = 1000 / this.fps;
+      const tolerance = 0.1;
+  
+      const animateLoop = (now) => {
+        this.requestID = requestAnimationFrame(animateLoop);
+        const delta = now - then;
+  
+        if (delta >= interval - tolerance) {
+          then = now - (delta % interval);
+          this.animate(delta);
+        }
+      };
+      this.requestID = requestAnimationFrame(animateLoop);
+    }
+  }
 
 class Canvas {
     constructor(width, height, cellSize) {
@@ -47,6 +80,8 @@ class Matrix extends Canvas {
     constructor(width, height, initialValue, cellSize) {
         super(width, height, cellSize);
         this.matrix = this.#init(initialValue);
+        this.live = [1, 3];
+        this.born = [1, 8];
     }
     
     #init(initialValue) {
@@ -56,7 +91,7 @@ class Matrix extends Canvas {
             const column = [];
 
             for (let j = 0; j < this.yCellsNumber; j++) {
-                column[j] = initialValue;
+                column[j] = initialValue(i, j);
             }
 
             row[i] = column;
@@ -65,14 +100,14 @@ class Matrix extends Canvas {
         return row
     }
 
-    iterate(callback = function(value, index, jndex) {}) {
-        for (let i = 0; i < this.xCellsNumber; i++) {
-            for (let j = 0; j < this.yCellsNumber; j++) {
-                callback(this.matrix[i][j], i, j)
+    static iterate(arr, callback = function(value, index, jndex) {}) {
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < arr[0].length; j++) {
+                callback(arr[i][j], i, j)
             }
         }
 
-        return this.matrix;
+        return this;
     }
 
     findCellPosition(xCord, yCord) {
@@ -92,24 +127,6 @@ class Matrix extends Canvas {
         return {xCord, yCord};
     }
 
-    mooreNeighborhood(xIndex = 0 , yIndex = 0) {
-        // 012
-        // 3C5
-        // 678
-
-        return [
-            xIndex - 1, yIndex - 1,   // 0
-            xIndex, yIndex - 1,       // 1
-            xIndex + 1, yIndex - 1,   // 2
-            xIndex - 1, yIndex,       // 3         
-                                      // 4
-            xIndex + 1, yIndex,       // 5
-            xIndex - 1, yIndex + 1,   // 6
-            xIndex, yIndex + 1,       // 7
-            xIndex + 1, yIndex + 1,   // 8
-        ]
-    }
-
     #findNeighbors(xIndex, yIndex, method) {
         let neighbors = 0;
         const count = method(xIndex, yIndex);
@@ -121,52 +138,83 @@ class Matrix extends Canvas {
             if (
                 x >= 0 && x < this.xCellsNumber &&   
                 y >= 0 && y < this.yCellsNumber &&
-                this.matrix[x][y] === `black`
+                !this.matrix[x][y].isDead
             ) neighbors ++;
         }
 
         return neighbors;
     }
 
-    step(live = [], born = [], method) {
-        const newMatrix = new Matrix(this.width, this.height,`white`,this.cellSize);
-        newMatrix.iterate((val, index, jndex) => {
-            newMatrix.matrix[index][jndex] = this.matrix[index][jndex];
-        })
+    clone() {
+      const matrixCopy = this.#init((i, j) => {
+          return this.matrix[i][j].clone();
+      })
 
-        this.iterate( (val, i ,j) => {
+      return matrixCopy;
+    }
+
+    step(method, newMatrix) {
+        Matrix.iterate(this.matrix, (val, i ,j) => {
             const neighborsCount = this.#findNeighbors(i, j, method);
+
+            if (val.isDead !== newMatrix[i][j].isDead) {
+              newMatrix.matrix[i][j].isDead = val.isDead;
+            }
+
             if (
-                val === `black` &&
-                (neighborsCount > live[1] ||
-                 neighborsCount < live[0])
+                !val.isDead  &&
+                (neighborsCount > this.live[1] ||
+                 neighborsCount < this.live[0])
             ) {
-                newMatrix.matrix[i][j] = `white`;
+                newMatrix[i][j].kill();
             }
             if (
-                val === `white` && 
-                neighborsCount >= born[0] &&
-                neighborsCount <= born[1]
+                val.isDead && 
+                neighborsCount >= this.born[0] &&
+                neighborsCount <= this.born[1]
             ) {
-                newMatrix.matrix[i][j] = `black`;
+                newMatrix[i][j].resurrect();
             }
             
             return
         })
-        this.matrix = newMatrix.matrix;
-        return this.matrix;
+        
+        return this
     }
 }
 
 const canvas = document.getElementById(`screen`);
-const wrapper = document.getElementById(`screen-wrapper`);
+const screenWrapper = document.getElementById(`screen-wrapper`);
 const startStop = document.getElementById(`start`);
-const initWidth = wrapper.clientWidth
-const initHeight = wrapper.clientHeight;
+const initWidth = screenWrapper.clientWidth
+const initHeight = screenWrapper.clientHeight;
 const ctx = canvas.getContext(`2d`);
-const CAGE_SIZE = 16;
+const CAGE_SIZE = 8;
 const BORDE_WIDTH = 1;
-const matrix = new Matrix(initWidth, initHeight, `white`, CAGE_SIZE);
+const initialCell = () => new Cell();
+const matrix = new Matrix(initWidth, initHeight, initialCell, CAGE_SIZE);
+const colors = new Colors();
+const animationFrame = new AnimationFrame();
+
+let live = [2, 3];
+let born = [3, 3];
+const mooreNeighborhood = (xIndex = 0 , yIndex = 0) => {
+  // 012
+  // 3C5
+  // 678
+
+  return [
+      xIndex - 1, yIndex - 1,   // 0
+      xIndex, yIndex - 1,       // 1
+      xIndex + 1, yIndex - 1,   // 2
+      xIndex - 1, yIndex,       // 3         
+                                // 4
+      xIndex + 1, yIndex,       // 5
+      xIndex - 1, yIndex + 1,   // 6
+      xIndex, yIndex + 1,       // 7
+      xIndex + 1, yIndex + 1,   // 8
+  ]
+}
 
 canvas.addEventListener('contextmenu', (event) => event.preventDefault());
 
@@ -174,44 +222,46 @@ ctx.canvas.height = matrix.height;
 ctx.canvas.width = matrix.width;
 ctx.imageSmoothingEnabled = false;
 ctx.lineWidth = BORDE_WIDTH;
-ctx.strokeStyle = `grey`;
-ctx.fillStyle = `white`;
+ctx.strokeStyle = colors.borderColor;
+ctx.fillStyle = colors.deadColor;
 
-const drawlogic = (ctx, matrix) => {
-    return (value, i, j) => {
-        const {xCord: x, yCord: y} = matrix.findCellCordinats(i, j);
+const drawlogic = (isInit, newMatrix) => { 
+  return (value, i, j) => {
+    const {xCord: x, yCord: y} = matrix.findCellCordinats(i, j);
+    const oldMatrixValue = matrix.matrix[i][j];
+    
+    if (!isInit && oldMatrixValue.isDead === value.isDead) return
 
-        ctx.fillStyle = value;
-        ctx.strokeRect(x, y, matrix.cellSize, matrix.cellSize);
-        ctx.fillRect(
-            x + BORDE_WIDTH, 
-            y + BORDE_WIDTH, 
-            matrix.cellSize - BORDE_WIDTH * 2, 
-            matrix.cellSize - BORDE_WIDTH * 2
-        );
-    }
+    ctx.fillStyle = colors.setColor(value.isDead);
+    ctx.strokeRect(x, y, matrix.cellSize, matrix.cellSize);
+    ctx.fillRect(
+        x + BORDE_WIDTH, 
+        y + BORDE_WIDTH, 
+        matrix.cellSize - BORDE_WIDTH * 2, 
+        matrix.cellSize - BORDE_WIDTH * 2
+    );
+  }
 }
 
-matrix.iterate(drawlogic(ctx, matrix));
-
 const draw = (event) => {
-    let drawColor;
+    let isDead;
     const {xIndex: x, yIndex: y} = matrix.findCellPosition(event.offsetX, event.offsetY);
-    const color = matrix.matrix[x][y];
+    const cell = matrix.matrix[x][y];
     
     switch(event.which) {
         case 1:
-            drawColor = `black`;
+            isDead = false
             break;
         case 3:
-            drawColor = `white`;
+            isDead = true;
             break;
         default:
            return  
     }   
-    if(color !== drawColor) {
-        drawlogic(ctx, matrix)(drawColor, x, y);
-        matrix.matrix[x][y] = drawColor;
+
+    if(cell.isDead !== isDead) {
+        matrix.matrix[x][y].isDead = isDead;
+        drawlogic(true)(matrix.matrix[x][y], x, y);
     }
 }
 
@@ -220,19 +270,35 @@ const startDraw = (event) => {
     canvas.addEventListener(`mousemove`, draw);      
 }
 
-const stopDraw = (event) => {
+const stopDraw = () => {
     canvas.removeEventListener(`mousemove`, draw); 
 }
 
 canvas.addEventListener('mousedown', startDraw);
 canvas.addEventListener('mouseup', stopDraw);
 
-startStop.addEventListener(`click`, event => {
-    setInterval(()=>{
-        console.time(`1`);
-        matrix.step([1, 3], [1, 8], matrix.mooreNeighborhood);
-        matrix.iterate(drawlogic(ctx, matrix));
-        console.timeEnd(`1`)
-    },500)
-       
-})
+const animate = () => {
+  console.time(`1`);
+  const newMatrix = matrix.clone();
+  matrix.step(mooreNeighborhood, newMatrix);
+  Matrix.iterate(newMatrix, drawlogic(false));
+  matrix.matrix = newMatrix;
+  console.timeEnd(`1`)
+}
+
+const start = () => {
+    let isPaused = false;
+
+    animationFrame.animate = animate;
+
+    return () => {
+        isPaused = !isPaused;
+
+        if (isPaused) animationFrame.start();
+        else animationFrame.stop();
+    }
+}
+
+startStop.addEventListener(`click`, start());
+
+Matrix.iterate(matrix.matrix, drawlogic(true));
